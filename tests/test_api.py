c382f6da6c9fd386c9a26f5986d78d5efbbe1b7a
Konstantin Kharitonov <64513267+sdw200t@users.noreply.github.com>
2022-10-20 22:55:11 +0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 
 URL = 'https://cloud-api.yandex.net/v1/disk/resources'
-TOKEN = 'y0_AgAAAAAQY8qsAADLWwAAAADMqLooynTY04MrTd2a7yImlfkbpBXWMjI'
+TOKEN = ''
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
 
 def test_createfolder():
